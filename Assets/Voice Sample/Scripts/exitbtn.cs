95b6f71e7cb5a426ed97d1e3619bbc316e040bdc
Amanuel <amantes30@nuaa.edu.cn>
2022-02-04 18:22:54 +0800

    sample scene update

---
+++ Assets/Voice Sample/Scripts/exitbtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitbtn : MonoBehaviour
+{
+   public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
